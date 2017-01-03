1d99bd398cf5ffb5f84876f9563d0d2ee672528d
yl007 <179068676@qq.com>
2017-01-03 22:39:25 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/ttweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.example.ttweather.model;
+
+/**
+ * Created by 磊 on 2017/1/3.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
